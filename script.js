2d626a8a9e794408c0412c5b83919de13c0a5d1d
OAK Crypton <165552451+oakcrypton@users.noreply.github.com>
2024-04-29 10:28:06 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+   let telegramApp = window.Telegram.WebApp; // Получаем объект window.Telegram.WebApp Телеграмма
+
+   // Функция для обновления данных пользователя
+   function updateUserData() {
+      let userCard = document.getElementById("usercard");
+      let userData = telegramApp.initDataUnsafe.user;
+
+      // Создаем элементы для отображения данных пользователя
+      let fullName = document.createElement('p');
+      fullName.innerText = `${userData.first_name} ${userData.last_name}`;
+      userCard.appendChild(fullName);
+
+      let userId = document.createElement('p');
+      userId.innerText = `User ID: ${userData.id}`;
+      userCard.appendChild(userId);
+
+      let username = document.createElement('p');
+      username.innerText = `Username: ${userData.username}`;
+      userCard.appendChild(username);
+
+      let avatar = document.createElement('img');
+      avatar.src = userData.photo_url;
+      avatar.alt = "User Avatar";
+      avatar.id = "avatar";
+      userCard.appendChild(avatar);
+   }
+
+   // Обновляем данные пользователя при загрузке страницы
+   updateUserData();
+
+   // Добавляем обработчик события для кнопки увеличения баланса
+   let incrementButton = document.getElementById("incrementButton");
+   incrementButton.addEventListener('click', function(){
+      let balanceElement = document.getElementById("balance");
+      let currentBalance = parseInt(balanceElement.innerText.split(": ")[1]);
+      balanceElement.innerText = `Balance: ${currentBalance + 1}`;
+   });
